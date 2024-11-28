cc914585ca28aa1affa52138193c696da264e2a5
1F10220129 <s1f102201290@iniad.org>
2024-11-29 03:47:31 +0900

    django initial complete

--- app_folder/models.py
+++ app_folder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class SampleDB(models.Model):
+    class Meta:
+        db_table = 'sample_table' 
+        verbose_name_plural = 'sample_table'
+    sample1 = models.IntegerField('sample1', null=True, blank=True) 
+    sample2 = models.CharField('sample2', max_length=255, null=True, blank=True) 
