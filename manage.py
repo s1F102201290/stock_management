f4d891ef57981c8650383c8e09ffbaaa9b2e5283
1F10220129 <s1f102201290@iniad.org>
2025-02-12 07:16:16 +0900

    データベース修正 postgresSQL

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_config.settings')
@@ -15,8 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
     execute_from_command_line(sys.argv)
-
-
 if __name__ == '__main__':
     main()
